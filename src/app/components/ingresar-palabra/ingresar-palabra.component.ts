4da6d6407a2a4cca355a7393c93970a4d48d1efd
maurspi <mauriciospirilis@gmail.com>
2023-07-03 15:54:53 -0300

    Verifica correctamente el ingreso de las letras!

--- src/app/components/ingresar-palabra/ingresar-palabra.component.ts
+++ src/app/components/ingresar-palabra/ingresar-palabra.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { PalabraService } from 'src/app/providers/palabra.service';
 
-
 @Component({
   selector: 'app-ingresar-palabra',
   templateUrl: './ingresar-palabra.component.html',
   styleUrls: ['./ingresar-palabra.component.scss']
 })
 export class IngresarPalabraComponent {
-  arrCoincidencias: string[] = []; // esto es para corregir!
+  arrCoincidencias: string[] = [];
   arrAdivinar: string[] = [];
   intentos: string[] = [];
   imagen = [
@@ -28,95 +27,73 @@ export class IngresarPalabraComponent {
   juegoTerminado = false;
   juegoGanado = false;
 
-  /*constructor(private db: PalabraService){
-    this.db.getConexion().then( ()=>{
+  constructor(private db: PalabraService) {
+    /*this.db.getConexion().then(() => {
       console.log('conexion exitosa!');
       this.setPalabra(this.db.getPalabraAleatoria());
-    }).catch( (err)=>{
+    }).catch((err) => {
       console.log(err);
-    });
-  }*/
-  constructor(){}
+    });*/
+  }
 
-  setPalabra(palabra: string){
+  setPalabra(palabra: string) {
     this.palabraAdivinar = palabra;
     this.arrAdivinar = palabra.split('');
     console.log(this.arrAdivinar);
     this.setArrAdivinar();
   }
 
-  setArrAdivinar(){
-    this.arrAdivinar.forEach( (letra)=>{
+  setArrAdivinar() {
+    this.arrAdivinar.forEach((letra) => {
       this.arrCoincidencias.push('?');
     });
   }
-
-  /*revisarPalabra(){
-    let aciertos = 0;
-    this.intentos.push(this.letra);
-    this.arrAdivinar.forEach((letra, index: number)=>{
-      if(this.letra === letra){
-        this.arrCoincidencias[index] = letra;
-        aciertos++;
-      }
-    });
-    if(aciertos == 0){
-      this.vidas();
-    }
-    this.reset();
-    this.gameOver();
-  }
-  */
-
- //ESTE METODO LO USO EN LUGAR DE REVISAR PALABRA
 
   verificarLetra() {
     const letra = this.letra.toLowerCase();
     this.intentos.push(letra);
     let aciertos = 0;
-    this.arrAdivinar.forEach((letraAdivinar, index: number) => {
-      if (letra === letraAdivinar.toLowerCase()) {
-        this.arrCoincidencias[index] = letraAdivinar;
+    for (let i = 0; i < this.arrAdivinar.length; i++) {
+      if (letra === this.arrAdivinar[i].toLowerCase()) {
+        this.arrCoincidencias[i] = this.arrAdivinar[i];
         aciertos++;
       }
-    });
+    }
     if (aciertos === 0) {
       this.vidas();
     }
     this.reset();
     this.gameOver();
   }
-  vidas(){
-    this.idx ++;
+
+  vidas() {
+    this.idx++;
     console.log(this.idx);
   }
 
-  reset(){
-    setTimeout( ()=>{
+  reset() {
+    setTimeout(() => {
       this.letra = '';
-    },50);
+    }, 50);
   }
 
-  gameOver(){
-    if(this.idx > 5){
+  gameOver() {
+    if (this.idx > 5) {
       this.juegoTerminado = true;
       this.input = false;
-    }else{
-      this.ganarJuego();
+      this.ganarJuego(); // Llamada a ganarJuego() solo si el juego no ha terminado -- CORRECTO FUNCIONA OK!!
     }
   }
+  
+  
 
-  ganarJuego(){
-    let ganar = 0;
-    this.arrCoincidencias.forEach( (letra) => {
-      if(letra === "?"){
-        ganar++;
-      }
-    });
-    if(ganar < 1){
+  ganarJuego() {
+    const ganar = !this.arrCoincidencias.includes('?');
+    if (ganar) {
       this.input = false;
       this.juegoGanado = true;
     }
   }
-
+  
+  
 }
